5906eada4ca7d31e2eb2e1c378ed8f344d6fc583
jasonh1 <jasonho.ib@gmail.com>
2014-08-20 19:48:00 +0700

    case sensitive fixes

--- src/javascripts/app/libs/RGraph.js
+++ src/javascripts/app/libs/RGraph.js
@@ -1,9 +1,9 @@
 /* istanbul ignore next */
 define(function(require) {
     'use strict';
-     require('./Rgraph.common.core');
-     require('./Rgraph.common.dynamic');
-     require('./Rgraph.common.tooltips');
-     require('./Rgraph.line');
+     require('./RGraph.common.core');
+     require('./RGraph.common.dynamic');
+     require('./RGraph.common.tooltips');
+     require('./RGraph.line');
 
 });
